53cc4ad780728bb255889096f313e5cdd7510d75
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-22 19:18:02 -0300

    set up api communication and base prompt

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, Response
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+from time import sleep
 
 load_dotenv()
 
@@ -14,11 +15,46 @@ genai.configure(api_key=GEMINI_API)
 app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
+def generate_response(prompt):
+    max_retries = 1
+    attempt_count = 0
+
+    while True:
+        try:
+            # I updated this prompt to match your Financial Advisor context
+            system_instruction = """
+            You are a Financial Advisor chatbot.
+            You should only answer questions related to finance, budgeting, and investment markets.
+            If the user asks about unrelated topics, politely decline to answer.
+            """
+
+            model_config = {
+                "temperature" : 0.1,
+                "max_output_tokens" : 8192
+            }
+
+            llm = genai.GenerativeModel(
+                model_name= MODEL,
+                system_instruction=system_instruction,
+                generation_config=model_config
+            )
+
+            response = llm.generate_content(prompt)
+            return response.text
+
+        except Exception as error:
+            attempt_count += 1
+            if attempt_count >= max_retries:
+                return "Gemini Error: %s" % error
+
+            sleep(50)
+
 
 @app.route("/chat", methods=["POST"])
 def chat():
     prompt = request.json["msg"]
-    response = bot(prompt)
+    response = generate_response(prompt)
+    return response
 
 @app.route("/")
 def home():
